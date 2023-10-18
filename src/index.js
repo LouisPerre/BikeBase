29a18223ef3caf07e627fe3cffed4cd944104c08
Matteo <matteo.courquin@fortuneo.com>
2023-10-18 11:57:10 +0200

    refacto(formatData): :recycle: Format Bikes.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+const { bikes } = require('../data/bikes')
+
 function sayHello() {
-    return 'Hello';
+  return 'Hello';
+}
+
+function showBikes() {
+  console.log(bikes.kawasaki)
 }
 
 module.exports.sayHello = sayHello;
+module.exports.showBikes = showBikes;
