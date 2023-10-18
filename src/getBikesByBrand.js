5d475067c00a472c6d6a2123255d48bae7485d4f
Louis Perrenot <louis.perrenot@edu.devinci.net>
2023-10-18 14:17:14 +0200

    fix(tests): :pencil2: Fix missing const inside getBikesByBrand

--- src/getBikesByBrand.js
+++ src/getBikesByBrand.js
@@ -1,10 +1,10 @@
 const { bikes } = require("../data/bikes");
 
-err = "brand must be a string";
-
 module.exports = {
   getBikesByBrand: (brand) => {
+    const err = "brand must be a string";
     if (typeof brand !== "string") return { err };
+
     return bikes[brand];
   },
 };
