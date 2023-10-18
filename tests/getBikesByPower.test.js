df823cedef6a3082896050dfa68836e423b3b219
Louis Perrenot <louis.perrenot@edu.devinci.net>
2023-10-18 14:11:00 +0200

    test(GetBikesByPower): :white_check_mark: Add unit test for the function 'getBikesByPower'

---
+++ tests/getBikesByPower.test.js
@@ -0,0 +1,22 @@
+const { getBikesByPower } = require('../src/getBikesByPower')
+
+test('The function sayHello should return a object', () => {
+    const result = getBikesByPower(128);
+    expect(typeof result).toBe('object')
+    expect(result).toEqual({
+        motorcycles: [
+            {
+                model: 'Ninja ZX-6R',
+                year_launched: 1995,
+                latest_year: 1998,
+                cylinder: 636,
+                power_hp: 128,
+                weight_kg: 196,
+                torque_nm: 70.8,
+                type: 'Sport',
+                a2: false
+            }
+        ]
+    }
+    )
+})
