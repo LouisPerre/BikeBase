3ca539ef8bc4123fcdc7402eaf10343e935a0765
Louis Perrenot <louis.perrenot@edu.devinci.net>
2023-10-19 13:38:05 +0200

    fix(index): :ambulance: Fix export and import of index

---
+++ index.js
@@ -0,0 +1,8 @@
+import getAllBikes from "./src/getAllBikes";
+import getAllBikesA2 from "./src/getAllBikesA2";
+import getBikesByBrand from "./src/getBikesByBrand";
+import getBikesByPower from "./src/getBikesByPower";
+import getBikesByYear from "./src/getBikesByYear";
+
+const obj = {getAllBikes, getAllBikesA2, getBikesByBrand, getBikesByPower, getBikesByYear};
+export default obj;
